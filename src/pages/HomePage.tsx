7cd245bf05aa6d9bbeff67840a647efc7b0970cc
RaffiAkhdilputra <raffiakdilputra123@gmail.com>
2025-09-08 05:20:01 +0700

    Footer slicing complete

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -56,6 +56,15 @@ function HomePage() {
                     <ProductList />
                 </div>
             </main>
+
+            {/* footer */}
+            <footer className="w-full border-t bg-white">
+                <div className="flex flex-row justify-between items-center mx-auto h-16 max-w-7xl px-4 gap-6 py-5 text-sm text-gray-500">
+                    <p>&copy; 2025 Ecovera. All rights reserved.</p>
+                    <p>Universitas Negeri Surabaya</p>
+                </div>
+            </footer>
+
         </>
     );
 }
